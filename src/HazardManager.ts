13e6d44d021fe74b1a41d1c04086429f676e2ce1
komed3 <pk97@outlook.de>
2026-05-01 20:06:23 +0200

    create HazardManager class

--- src/HazardManager.ts
+++ src/HazardManager.ts
@@ -1,15 +1,6 @@
 import type { HazardStatements } from './types';
 
 export class HazardManager {
-  private map: HazardStatements = {
-    hazard: [],
-    precautionary: [],
-    eu: []
-  };
-
-  public build () : HazardStatements {
-    return this.map;
-  }
+  private map: HazardStatements = {};
+  private frozen: boolean = false;
 }
-
-export const HP = () => new HazardManager();
