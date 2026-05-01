0310e43dc24b6aae933fb7340e99baa71a8307f3
komed3 <pk97@outlook.de>
2026-05-01 23:52:01 +0200

    implement cache + define fallback language

--- src/translate.ts
+++ src/translate.ts
@@ -1,1 +1,7 @@
-export class Translate {}
+import type { StatementCode } from '../data';
+import type { LangCode, StatementConfig } from './types';
+
+export class Translate {
+  private static cache = new Map< StatementCode, StatementConfig >();
+  private static fallback: LangCode = 'en';
+}
