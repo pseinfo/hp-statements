2827675db2a82ba2c5498c03e63c9b55f63c24cf
komed3 <pk97@outlook.de>
2026-05-01 23:53:50 +0200

    get prefix from given code

--- src/translate.ts
+++ src/translate.ts
@@ -1,7 +1,11 @@
 import type { StatementCode } from '../data';
-import type { LangCode, StatementConfig } from './types';
+import type { LangCode, StatementConfig, StatementType } from './types';
 
 export class Translate {
   private static cache = new Map< StatementCode, StatementConfig >();
   private static fallback: LangCode = 'en';
+
+  private static prefix ( code: StatementCode ) : StatementType {
+    return code.match( /^[A-Z]+/ )?.[ 0 ]! as StatementType;
+  }
 }
