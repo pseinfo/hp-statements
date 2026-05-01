a7b2e4e13d6e6b910c911a72e318d6242623e21d
komed3 <pk97@outlook.de>
2026-05-01 23:55:18 +0200

    load statement config from code

--- src/translate.ts
+++ src/translate.ts
@@ -8,4 +8,19 @@ export class Translate {
   private static prefix ( code: StatementCode ) : StatementType {
     return code.match( /^[A-Z]+/ )?.[ 0 ]! as StatementType;
   }
+
+  private static async load ( code: StatementCode ) : Promise< StatementConfig | undefined > {
+    if ( this.cache.has( code ) ) return this.cache.get( code );
+
+    const prefix = this.prefix( code );
+    if ( ! prefix ) return;
+
+    try {
+      const mod = await import( `../data/${ prefix }/${ code }.ts` );
+      const data = mod.default as StatementConfig;
+
+      this.cache.set( code, data );
+      return data;
+    } catch { return }
+  }
 }
