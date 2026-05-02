da9fe224595efae40217629376fb114f01958e18
komed3 <pk97@outlook.de>
2026-05-02 10:29:58 +0200

    implement statement code type guards

--- src/assert.ts
+++ src/assert.ts
@@ -11,4 +11,20 @@ export class Assert {
     if ( typeof code !== 'string' ) return undefined;
     return code.match( /^[A-Z]+/ )?.[ 0 ];
   };
+
+  public static isHCode ( value: unknown ) : value is HCode {
+    return CODES.H.has( value as HCode );
+  }
+
+  public static isPCode ( value: unknown ) : value is PCode {
+    return CODES.P.has( value as PCode );
+  }
+
+  public static isEUHCode ( value: unknown ) : value is EUHCode {
+    return CODES.EUH.has( value as EUHCode );
+  }
+
+  public static isStatementCode ( value: unknown ) : value is StatementCode {
+    return CODES.ALL.has( value as StatementCode );
+  }
 }
