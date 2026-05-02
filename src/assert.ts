ff4ec637a364d722f7a269bde8c57247086c9f34
komed3 <pk97@outlook.de>
2026-05-02 10:30:54 +0200

    assert H/P/EUH codes

--- src/assert.ts
+++ src/assert.ts
@@ -27,4 +27,24 @@ export class Assert {
   public static isStatementCode ( value: unknown ) : value is StatementCode {
     return CODES.ALL.has( value as StatementCode );
   }
+
+  public static assertHCode ( value: unknown ) : HCode | never {
+    if ( this.isHCode( value ) ) return value;
+    throw new Error( `Invalid H-Code: got "${ value as string }", expected valid H statement code` );
+  }
+
+  public static assertPCode ( value: unknown ) : PCode | never {
+    if ( this.isPCode( value ) ) return value;
+    throw new Error( `Invalid P-Code: got "${ value as string }", expected valid P statement code` );
+  }
+
+  public static assertEUHCode ( value: unknown ) : EUHCode | never {
+    if ( this.isEUHCode( value ) ) return value;
+    throw new Error( `Invalid EUH-Code: got "${ value as string }", expected valid EUH statement code` );
+  }
+
+  public static assertStatementCode ( value: unknown ) : StatementCode | never {
+    if ( this.isStatementCode( value ) ) return value;
+    throw new Error( `Invalid Statement Code: got "${ value as string }", expected valid H/P/EUH statement code` );
+  }
 }
