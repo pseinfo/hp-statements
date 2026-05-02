ddcd4b4564cdd93b3f2c84bf77b571f38eaaf250
komed3 <41899980+komed3@users.noreply.github.com>
2026-05-02 15:08:09 +0000

    data: update ghs statements from source

---
+++ data/H/H360Fd.ts
@@ -0,0 +1,30 @@
+import type { StatementConfig } from '../../src/types';
+
+export default ( {
+  code: 'H360Fd',
+  translations: {
+    bg: 'Може да увреди оплодителната способност. Предполага се, че уврежда плода.',
+    cs: 'Může poškodit reprodukční schopnost. Podezření na poškození plodu v těle matky.',
+    da: 'Kan skade forplantningsevnen. Mistænkes for at skade det ufødte barn.',
+    de: 'Kann die Fruchtbarkeit beeinträchtigen. Kann vermutlich das Kind im Mutterleib schädigen.',
+    el: 'Μπορεί να βλάψει τη γονιμότητα. Ύποπτο για πρόκληση βλάβης στο έμβρυο.',
+    en: 'May damage fertility. Suspected of damaging the unborn child.',
+    es: 'Puede perjudicar a la fertilidad. Se sospecha que daña al feto.',
+    et: 'Võib kahjustada viljakust. Arvatavasti kahjustab loodet.',
+    fi: 'Saattaa heikentää hedelmällisyyttä. Epäillään vaurioittavan sikiötä.',
+    fr: 'Peut nuire à la fertilité. Susceptible de nuire au fœtus.',
+    hr: 'Može štetno djelovati na plodnost. Sumnja na mogućnost štetnog djelovanja na nerođeno dijete.',
+    hu: 'Károsíthatja a termékenységet. Feltehetően károsítja a születendő gyermeket.',
+    it: 'Può nuocere alla fertilità. Sospettato di nuocere al feto.',
+    lt: 'Gali pakenkti vaisingumui. Įtariama, kad gali pakenkti negimusiam kūdikiui.',
+    lv: 'Var negatīvi ietekmēt auglību. Ir aizdomas, ka var nodarīt kaitējumu nedzimušam bērnam.',
+    mt: 'Jista\' jagħmel ħsara lill-fertilità. Suspettat li jagħmel ħsara lit-tarbija li għadha ma twelditx.',
+    nl: 'Kan de vruchtbaarheid schaden. Wordt ervan verdacht het ongeboren kind te schaden.',
+    pl: 'Może działać szkodliwie na płodność. Podejrzewa się, że działa szkodliwie na dziecko w łonie matki.',
+    pt: 'Pode afectar a fertilidade. Suspeito de afectar o nascituro.',
+    ro: 'Poate dăuna fertilității. Susceptibil de a dăuna fătului.',
+    sk: 'Môže poškodiť plodnosť. Podozrenie z poškodzovania nenarodeného dieťaťa.',
+    sl: 'Lahko škoduje plodnosti. Sum škodljivosti za nerojenega otroka.',
+    sv: 'Kan skada fertiliteten. Misstänks kunna skada det ofödda barnet.'
+  }
+} ) as const satisfies StatementConfig;
